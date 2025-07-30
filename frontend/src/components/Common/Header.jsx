6ed7eba9a92d0a956b262d7cb701273d1eea43fb
Wiruni <163153212+Wiruni@users.noreply.github.com>
2025-07-31 01:50:35 +0530

    Your message here

--- frontend/src/components/Common/Header.jsx
+++ frontend/src/components/Common/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div>
+    <header>
         {/* Topbar */}
         <Topbar />
-        {/* navbar */}
+        {/* Navbar */}
+        <Navbar />
         {/* Cart Drawer */}
-    </div>
+    </header>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "stylenest-green": "#005249",
+     }
+    },
   },
   plugins: [],
 }
